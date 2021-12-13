af9fbf46f28b5ce554bb99fb5a9a3482872b796c
ryan0000 <rlibed@esri.com>
2021-12-12 22:16:36 -0800

    Updates

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 
-// Calcite Components
 import "@esri/calcite-components/dist/calcite/calcite.css";
 import "@esri/calcite-components/dist";
 import {
@@ -22,9 +20,4 @@ import {
     </React.StrictMode>,
     document.getElementById("root")
   );
-
-  // If you want to start measuring performance in your app, pass a function
-  // to log results (for example: reportWebVitals(console.log))
-  // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-  reportWebVitals();
 })();
